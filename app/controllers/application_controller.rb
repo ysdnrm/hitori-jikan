073e32909bf8e533e9e57cbcc8d1830569c34f33
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-08-08 22:43:11 +0000

    [Add] ユーザー認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  # ユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
