e84b41114644f2f63a9d701dfcdb4e30b05baac5
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-08-09 23:52:15 +0000

    [Add] postsコントローラ

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,45 @@
+class PostsController < ApplicationController
+    
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    # current_user は、ログイン中のユーザーの情報を取得できる便利な記述(devise を導入しないと使用することができない)
+    @post.user_id = current_user.id
+   if @post.save
+      redirect_to posts_path
+   else
+      render :new
+   end  
+  end
+
+  def index
+    # 1ページ分の決められた数のデータだけを、新しい順に取得するように変更
+    @posts = Post.page(params[:page])
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+    # @post_comment = PostComment.new
+  end
+  
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+  
+  
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_params
+    params.require(:post).permit(:shop_name, :shop_introduction, :shop_postal_code, :shop_address, :stay_weekday, :stay_time_start, :stay_time_end, :congestion_degree)
+  end
+  
+end
+
+end
+
--- db/migrate/20230809041930_create_posts.rb
+++ db/migrate/20230809041930_create_posts.rb
@@ -1,11 +1,15 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.integer :user_id
-      t.string :shop_name
-      t.text :caption
-      t.string :postal_code,      null: false
-      t.string :address,          null: false
+      t.integer :user_id,              null: false
+      t.string :shop_name,             null: false
+      t.text :shop_introduction,       null: false
+      t.string :shop_postal_code,      null: false
+      t.string :shop_address,          null: false
+      t.integer :stay_weekday,         null: false
+      t.integer :stay_time_start,      null: false
+      t.integer :stay_time_end,        null: false
+      t.integer :congestion_degree,    null: false, default: "0"
       t.timestamps
     end
   end
