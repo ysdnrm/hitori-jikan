104c8ff54f332b4a47873a0d9a34895d2137a021
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-09-18 11:39:47 +0000

    [Add] 住所自動入力

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
    before_action :authenticate_user!, except: [:index, :show]
 
   def new
+    # byebug
     @post = Post.new
   end
 
@@ -19,7 +20,7 @@ class PostsController < ApplicationController
     else
       render 'new'
     end
-
+# byebug
   end
 
   # 下書き
