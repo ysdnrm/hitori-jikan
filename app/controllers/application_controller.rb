5f12086a69a1cfda18e72decf4bb9ba89addfff5
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-10-11 14:59:00 +0000

    [Fix] 不要コード削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
@@ -9,7 +8,7 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path #トップページ
   end
-  
+
   def after_sign_up_path_for(resource)
     root_path #マイページ
   end
