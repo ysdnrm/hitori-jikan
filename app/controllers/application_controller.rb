af011c0228dc964306c8399529eeb35f75db9984
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-10-21 15:16:54 +0000

    [Fix] 不要コード削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    root_path #マイページ
+    root_path #トップページ
   end
 
   def after_sign_out_path_for(resource)
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_up_path_for(resource)
-    root_path #マイページ
+    root_path #トップページ
   end
 
   protected
